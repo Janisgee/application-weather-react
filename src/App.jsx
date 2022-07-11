6d3d42afc252efd17586e4dde5fc2d24b0fc8c39
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-07-11 10:17:41 +0800

    Added the GitHub repo URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App </div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App{" "}
+      <footer>
+        This project was coded by{" "}
+        <a href="https://janischan-portfolio-jc.netlify.app" target="_blank">
+          Janis Chan
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Janisgee/application-weather-react"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub
+      </footer>
+    </div>
+  );
 }
