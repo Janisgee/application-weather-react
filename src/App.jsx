208719c50b959a433526b52c6390ab751b21cf34
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-07-11 10:46:14 +0800

    Added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,24 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      Weather App{" "}
-      <footer>
-        This project was coded by{" "}
-        <a href="https://janischan-portfolio-jc.netlify.app" target="_blank">
-          Janis Chan
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Janisgee/application-weather-react"
-          target="_blank"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className="container">
+        {" "}
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://janischan-portfolio-jc.netlify.app" target="_blank">
+            Janis Chan
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Janisgee/application-weather-react"
+            target="_blank"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
